44023a8f2b2a961259b68f4ca1c673b3104081f6
de2 <de2@WIN11-95>
2026-06-23 14:55:58 +0200

    css und links

--- Labs/Lab02_JS/script.js
+++ Labs/Lab02_JS/script.js
@@ -1,10 +1,10 @@
 function getBiggerNumber(n1, n2) {
-    if (n1>n2)
-        return n1
+    if (n1>n2) 
+        return n1;
     else if (n2>n1)
-        return n2
+        return n2;
     else
-        return NaN
+        return NaN;
 }
 
 biggerNumber.addEventListener("submit", function(event) {
